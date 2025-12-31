a3ef3c5869b4dda03e45c98213074289e914d5c7
risqi <risqiRPL@gmail.com>
2025-12-31 20:52:47 +0700

    Fix legend scrollbar visibility: add proper CSS styles for webkit and firefox scrollbars

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -278,6 +278,30 @@
     background: rgba(255, 255, 255, 0.2);
     color: white;
 }
+
+/* Legend scrollbar styles */
+.info.legend::-webkit-scrollbar {
+    width: 8px;
+}
+
+.info.legend::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 4px;
+}
+
+.info.legend::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 4px;
+}
+
+.info.legend::-webkit-scrollbar-thumb:hover {
+    background: #555;
+}
+
+.info.legend {
+    scrollbar-width: thin;
+    scrollbar-color: #888 #f1f1f1;
+}
 </style>
 @endpush
 
